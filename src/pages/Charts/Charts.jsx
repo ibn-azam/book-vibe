4b7a69b1f55516a259a221cdc14fea688e4b6cbc
ibn-azam <farhan.tsx@gmail.com>
2026-04-28 20:32:04 +0100

    added pages in charts

--- src/pages/Charts/Charts.jsx
+++ src/pages/Charts/Charts.jsx
@@ -1,28 +1,32 @@
 import React, { use } from 'react';
-import { Bar, BarChart, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
 
-const Charts = ({booksPromise}) => {
-     
-    const booksDataRes = use(booksPromise);
-    const booksData = booksDataRes.data;
+const Charts = ({ booksPromise }) => {
+  const booksDataRes = use(booksPromise);
+  const booksData = booksDataRes.data;
 
-    const booksChartData = booksData.map(bookData =>{
-        const book ={
-            id: bookData.bookId,
-            name: bookData.bookName,
-            rating : bookData.rating
-        }
-        return book;
-    })
-    return (
-        <div className='container mx-auto flex justify-center items-center my-10 bg-gray-100 py-10'>
-            <BarChart width={500} height={400} data={booksChartData}>
-                <XAxis dataKey="name"></XAxis>
-                <YAxis></YAxis>
-                <Bar dataKey="rating" fill='red'></Bar>
-            </BarChart>
-        </div>
-    );
+  const booksChartData = booksData.map(bookData => ({
+    id: bookData.bookId,
+    name: bookData.bookName,
+    rating: bookData.rating,
+    pages: bookData.totalPages,
+  }));
+
+  return (
+    <div className='container mx-auto p-6'>
+      <h2 className='text-2xl text-center font-bold mb-6'>Books Rating Chart</h2>
+      <ResponsiveContainer width='100%' height={400}>
+        <BarChart data={booksChartData}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='name' />
+          <YAxis domain={[0, 5]} />
+          <Tooltip />
+          <Bar dataKey='rating' fill='#a78bfa' radius={[4, 4, 0, 0]} />
+          <Bar dataKey='pages' fill='#2B7FFF' radius={[4, 4, 0, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Charts;
